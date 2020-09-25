e60811755cc10125692713db24f7e85704317c24
Naga Bhargavi Marudhuri <bhargavimarudhuri@gmail.com>
2020-09-25 02:28:17 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -8,7 +8,7 @@ num1-num2
 end
 
 def division(num1, num2)
-return num2 / num1
+return num1 / num2
 end
 
 def multiplication(num1, num2)
@@ -16,7 +16,7 @@ return num1 * num2
 end
 
 def modulo(num1, num2)
- num2 % num1
+ num1 % num2
 end
 
 def square_root(num)
